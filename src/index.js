cd58f42b9dd1e4ef5b6a68d036d391d6c71be71b
krishna <krishna210706@gmail.com>
2026-06-23 04:47:27 +0530

    utils and merging database

---
+++ src/index.js
@@ -0,0 +1,39 @@
+//require('dotenv').config({path : './env'})
+
+import dotenv from 'dotenv'
+import connectDB from './db/index.js';
+
+dotenv.config({
+  path: './env'
+})
+connectDB()
+.then(()=>{
+  app.listen(process.env.port || 8000,()=>{
+    console.log(`server is running at port ${process.env.PORT}`)
+  })
+})
+.catch((err)=>{
+  console.log("MONGODB connection failed",err);
+})
+
+
+/*
+import express from 'express'
+
+(async ()=>{
+  try{
+    mongoose.connect(`${process.env.
+      MONGODB_URI}/${DB_NAME}`)
+      app.on("error",(error)=>{
+        console.log("ERRR:",error);
+        throw error
+      })
+
+      app.listen(process.env.PORT,()=>{
+        console.log(`app is listening on this port ${process.env.PORT}`);
+      })
+  } catch (error){
+    console.error("ERROR",error)
+  }
+})()
+  */
