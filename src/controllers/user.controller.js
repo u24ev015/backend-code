51ec68ae413cbed6bd5445e761964f016d93e4c9
krishna <krishna210706@gmail.com>
2026-06-30 12:15:55 +0530

    add comment ,tweet, like , playlist model

--- src/controllers/user.controller.js
+++ src/controllers/user.controller.js
@@ -153,8 +153,8 @@ const loginUser = asyncHandler(async (req,res)=>{
 
 const logoutUser = asyncHandler(async (req,res)=>{
   await User.findByIdAndUpdate(req.user._id,{
-    $set:{
-      refreshToken:undefined
+    $unset:{
+      refreshToken:1 // this removes field from this document
     }
   },
     {
